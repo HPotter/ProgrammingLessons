6c1afdbf4fd31bb8b200817cc0db297b7dcbce83
Anton Blintsov <blint@narod.ru>
2012-03-01 14:48:38 +0400

    [pre-cycle] Added functors and templates. What else?

--- pre-cycle/main.cpp
+++ pre-cycle/main.cpp
@@ -2,75 +2,70 @@
 
 using namespace std;
 
-
-const unsigned int DEFAULT_N = 42;
-
+template <class T>
 class Function {
 public:
-        Function(unsigned int _n) {
-                if(n != 0) {
-                        n = _n;
-                } else {
-                        Function();
-                }
-        }
-        Function() {
-                n = DEFAULT_N;
-        }
-        unsigned int operator()(unsigned int x) {
-                return myfunc(1, x);
-        }
-        unsigned int operator()(unsigned int pow, unsigned int x) { 
-               return myfunc(pow, x);
-        }
+	Function(T _n) : n(_n) {}
+	T operator()(T x) {
+		return x*x % n;
+	}
 private:
-        //FUNCTION IS HERE
-        unsigned int function(unsigned int x) {
-                return x*x % n;
-        }
+	T n;
+};
 
-        unsigned int myfunc(unsigned int pow, unsigned int x) {
-                unsigned int result = 0;
-                if(pow > 0) {
-                        result = myfunc(pow-1, function(x)) % n;
-                } else if(pow == 0) {
-                        result = x % n;
-                }
-                
-                return result;
-        }
-        unsigned int n; 
+template <class T>
+class FunctionWrapper {
+public:
+	FunctionWrapper(Function<T>& _func) : func(_func) {}
+	T operator()(T x) {
+		return func(x);
+	}
+	T operator()(unsigned int pow, T x) {
+		return myfunc(pow, x);
+	}
+private:
+	T myfunc(unsigned int pow, T x) {
+		T result = 0;
+		if(pow > 0) {
+			result = myfunc(pow-1, func(x));
+		} else if(pow == 0) {
+			result = x;
+		}
+		return result;
+	}
+	Function<T> func;
 };
 
 int main() {
-        unsigned int n = 0;
-        cout << "Modulus:" << endl;
-        cin >> n;
-        Function f(n);
-        
-        unsigned int initial = 0;
-        cout << "Initial number:" << endl;
-        cin >> initial;
+	unsigned int n = 0;
+	cout << "Modulus:" << endl;
+	cin >> n;
+	Function<unsigned int> func(n);
+	FunctionWrapper<unsigned int> f(func);
 
-        unsigned int in_cycle = f(2*n, initial);
-        
-        unsigned int counter = 0;
-        unsigned int pointer = in_cycle;
-        do {
-                pointer = f(pointer);
-                counter++;
-        } while(pointer != in_cycle);
-        
-        unsigned int result = 0;
-        unsigned int initial_value = initial;
-        unsigned int counter_value = f(counter, initial);
-        while(initial_value != counter_value) {
-                result++;
-                initial_value = f(initial_value);
-                counter_value = f(counter_value);
-        }
+	unsigned int initial = 0;
+	cout << "Initial number:" << endl;
+	cin >> initial;
 
-        cout << "Result: " << result << endl;
+	unsigned int in_cycle = f(2*n, initial);
 
-        return 0;
+	unsigned int counter = 0;
+	unsigned int pointer = in_cycle;
+	do {
+	pointer = f(pointer);
+	counter++;
+	} while(pointer != in_cycle);
+
+	unsigned int result = 0;
+	unsigned int initial_value = initial;
+	unsigned int counter_value = f(counter, initial);
+	while(initial_value != counter_value) {
+	result++;
+	initial_value = f(initial_value);
+	counter_value = f(counter_value);
+	}
+
+	cout << "Result: " << result << endl;
+
+	return 0;
 }
