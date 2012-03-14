c8937a6eafe07e4b4c76e32e53929190678bd74b
Anton Blintsov <blint@narod.ru>
2012-03-14 19:03:22 +0400

    Working, with separate function, const's, etc.

--- pre-cycle/main.cpp
+++ pre-cycle/main.cpp
@@ -2,29 +2,51 @@
 
 using namespace std;
 
+const unsigned int defaultCardinality = 42;
+
 template <class T>
 class Function {
 public:
-	Function(T _n) : n(_n) {}
-	T operator()(T x) {
-		return x*x % n;
+	T operator()(T x) const {
+		return x*x;
 	}
-private:
-	T n;
 };
 
-template <class T>
+template <int modulus>
+class ModulusInteger {
+public:
+	ModulusInteger(int _value) : value(_value % modulus) {}
+	unsigned int operator * (ModulusInteger another) const {
+		return value*another.value % modulus;
+	}
+	void operator = (ModulusInteger another) {
+		value = another.value;
+	}
+	bool operator == (ModulusInteger another) const {
+		return value == another.value;
+	}
+	bool operator != (ModulusInteger another) const {
+		return value != another.value;
+	}
+	unsigned int domainCardinality() const {
+		return modulus;
+	}
+private:
+	unsigned int value;
+};
+
+template <class T, template <class> class F>
 class FunctionWrapper {
 public:
-	FunctionWrapper(Function<T>& _func) : func(_func) {}
-	T operator()(T x) {
+	FunctionWrapper(const F<T>& _func) : func(_func) {}
+	T operator()(const T& x) const {
 		return func(x);
 	}
-	T operator()(unsigned int pow, T x) {
+	T operator()(const unsigned int pow, const T& x) const {
 		return myfunc(pow, x);
 	}
 private:
-	T myfunc(unsigned int pow, T x) {
+	T myfunc(const unsigned int pow, const T& x) const {
 		T result = 0;
 		if(pow > 0) {
 			result = myfunc(pow-1, func(x));
@@ -33,38 +55,44 @@ private:
 		}
 		return result;
 	}
-	Function<T> func;
+	F<T> func;
 };
 
-int main() {
-	unsigned int n = 0;
-	cout << "Modulus:" << endl;
-	cin >> n;
-	Function<unsigned int> func(n);
-	FunctionWrapper<unsigned int> f(func);
+template <class T, template <class> class F>
+unsigned int preCycleLength(const T& initial, const F<T>& function) {
+	FunctionWrapper< T, F > f(function);
+	unsigned int cardinality = initial.domainCardinality();
+	T inCycle = f(2*cardinality, initial);
 
-	unsigned int initial = 0;
-	cout << "Initial number:" << endl;
-	cin >> initial;
-
-	unsigned int in_cycle = f(2*n, initial);
-
-	unsigned int counter = 0;
-	unsigned int pointer = in_cycle;
+	unsigned int cycleLength = 0;
+	T pointer = inCycle;
 	do {
-	pointer = f(pointer);
-	counter++;
-	} while(pointer != in_cycle);
+		pointer = f(pointer);
+		cycleLength++;
+	} while(pointer != inCycle);
 
 	unsigned int result = 0;
-	unsigned int initial_value = initial;
-	unsigned int counter_value = f(counter, initial);
-	while(initial_value != counter_value) {
-	result++;
-	initial_value = f(initial_value);
-	counter_value = f(counter_value);
+	T leftValue = initial;
+	T rightValue = f(cycleLength, initial);
+	while(leftValue != rightValue) {
+		result++;
+		leftValue = f(leftValue);
+		rightValue = f(rightValue);
 	}
 
+	return result;
+}
+
+int main() {
+	int init;
+	cin >> init;
+
+	const int mod = 6;
+
+	ModulusInteger<mod> initial(init);
+	Function< ModulusInteger<mod> > func;
+
+	unsigned int result =  preCycleLength(initial, func);
 	cout << "Result: " << result << endl;
 
 	return 0;
